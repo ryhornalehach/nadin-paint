37ac2d90ac53fb4d1740b88f0e3eda63a1a32b97
ryhornalehach <ryhornalehach@icloud.com>
2018-01-31 12:47:57 -0500

    photo gallery styling

--- react/src/components/ShowroomTile.js
+++ react/src/components/ShowroomTile.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ShowroomTile = props => {
   let photo;
   if (props.photo) {
-    photo = <img src={props.photo} data-caption={props.text} onClick={props.handleImagePush} id={props.index}></img>
+    photo = <img src={props.photo} data-caption={props.text} onClick={props.handleImagePush} id={props.index} className="img-box"></img>
   }
 
   return (
